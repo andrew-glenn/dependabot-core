750c8f8035c14094b61869cfde0699bc592046f2
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2021-06-12 19:54:33 -0500

    Adding tag support to git submodules/update checker

--- git_submodules/lib/dependabot/git_submodules/update_checker.rb
+++ git_submodules/lib/dependabot/git_submodules/update_checker.rb
@@ -9,7 +9,7 @@ module Dependabot
   module GitSubmodules
     class UpdateChecker < Dependabot::UpdateCheckers::Base
       def latest_version
-        @latest_version ||= fetch_latest_version
+        return latest_version_for_git_dependency
       end
 
       def latest_resolvable_version
@@ -39,13 +39,30 @@ module Dependabot
         raise NotImplementedError
       end
 
-      def fetch_latest_version
-        git_commit_checker = Dependabot::GitCommitChecker.new(
-          dependency: dependency,
-          credentials: credentials
-        )
+      def latest_version_for_git_dependency
+        # If the module isn't pinned then there's nothing for us to update
+        # (since there's no lockfile to update the version in). We still
+        # return the latest commit for the given branch, in order to keep
+        # this method consistent
+        return git_commit_checker.head_commit_for_current_branch unless git_commit_checker.pinned?
 
-        git_commit_checker.head_commit_for_current_branch
+        # If the dependency is pinned to a tag that looks like a version then
+        # we want to update that tag. Because we don't have a lockfile, the
+        # latest version is the tag itself.
+        if git_commit_checker.pinned_ref_looks_like_version?
+          latest_tag = git_commit_checker.local_tag_for_latest_version&.
+                       fetch(:tag)
+          version_rgx = GitCommitChecker::VERSION_REGEX
+          return unless latest_tag.match(version_rgx)
+
+          version = latest_tag.match(version_rgx).
+                    named_captures.fetch("version")
+          return version_class.new(version)
+        end
+
+        # If the dependency is pinned to a tag that doesn't look like a
+        # version then there's nothing we can do.
+        nil
       end
     end
   end
